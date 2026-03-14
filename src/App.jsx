a8c76a8d8cc8fb2def85f44ca5f31aeb2b92a9f3
Johirul Islam Himel <himel1199@gmail.com>
2026-03-14 20:28:32 +0600

    Load dynamic data, API call using use()

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import Counter from "./Counter";
-import "./App.css";
 import Batsman from "./Batsman";
+import User from "./User";
+import Friends from "./Friends";
+import "./App.css";
+import { Suspense } from "react";
 
+// user data fetching
+const fetchUser = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json(),
+);
+
+// Friends data fetching
+const fetchFriends = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
 function App() {
+  const friendsPromise = fetchFriends();
   function handleClick() {
     alert("I am clicked");
   }
@@ -18,6 +32,16 @@ function App() {
   return (
     <>
       <h3>Get started Vite + React</h3>
+
+      {/* user component */}
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <User fetchUser={fetchUser}></User>
+      </Suspense>
+      {/* friends component */}
+      <Suspense fallback={<h3>Friends are coming for treat... </h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
+      {/* Batsman Component */}
       <Batsman></Batsman>
       <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
